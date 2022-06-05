173fec96942d1e9de160bb096cddde2fccea2390
ezraa1 <ezrakipchirchir1034@gmail.com>
2022-06-05 15:02:29 +0300

    add onclick button

--- profile.js
+++ profile.js
@@ -1,11 +1,13 @@
-const navbar = document.querySelector('.navbar');
+// const navbar = document.querySelector('.navbar');
 
-window.onscroll = function() {
-    var top = window.scrollY;
-    if(top>=100) {
-        navbar.classList.add('navbarDark');
-    }
-    else {
-        navbar.classList.remove('navbarDark');
-    }
-}
+// window.onscroll = function() {
+//     var top = window.scrollY;
+//     if(top>=100) {
+//         navbar.classList.add('navbarDark');
+//     }
+//     else {
+//         navbar.classList.remove('navbarDark');
+//     }
+// }
+let text = ['Learn Basic Java Skill sets.','Design a web using HTML CSS & js.','Basic introduction to Software and Hardware.Operating Systems,Networking and Trouble-Shooting']
+document.getElementById("cert").innerHTML = text;
